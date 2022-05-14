f1446236109c86ae15af51d33437c41366ad7cce
Elizaveta <vakhlakova01@mail.ru>
2022-05-14 16:42:14 -0700

    update

--- index.js
+++ index.js
@@ -90,28 +90,55 @@ let option1 = document.getElementById("first")
 let option2 = document.getElementById("second")
 let option3 = document.getElementById("third")
 let option4 = document.getElementById("forth")
+let header = document.getElementById("header")
+
+//access to this object ONLY FOR THIS FILE
+const additionInfo =  {
+    emojis: ["💚", "💛", "💙", "🧡"],
+    // wishes: ["Smile more!", "You can do everything!", "If you have a bad day, it is not the whole life!",
+    //  "Don't be afraid of new things!"],
+     get KindWish(){
+         return `Select font and get a piece of advice ${this.emojis[0]}`
+     },
+     set KindWish(value){ //1 parameter
+          header.innerHTML = value //only in this way or it will be redirected to GET VALUE IS NOT ``
+     }
+
+}
+
+header.innerHTML = additionInfo.KindWish
+
+
 
 function newfont (){
     if(option1.selected){ //selectes - property of this object
         input.style.background = "silver"
-        input.style.fontFamily = "sans-serif"
+        input.style.fontFamily = "sans-serif" 
+        // let x = additionInfo.KindWish = `<span style = "margin-left: 83px">Smile more!💛</span>`  //it will be by default VARIABLE WITH PROPERTY
+        // header.innerHTML = x   //as a storage
+        additionInfo.KindWish = `<span style = "margin-left: 83px">Smile more!💛</span>`
     }
     else if(option2.selected){
         input.style.background = "rgb(245, 238, 213)"
         input.style.fontFamily = "monospace"
+        additionInfo.KindWish = `<span style = "margin-left: 53px">You can do everything!💙</span>`
     }
     else if(option3.selected){ //select NOT value, not option!
         input.style.fontFamily = "fantasy"
         input.style.background = "rgb(240, 220, 149)"
+        additionInfo.KindWish = `<span style = "margin-left: -26px">If you have a bad day, it is not the whole life!🧡</span>`
     }
     else if(option4.selected){    //<select> - selected
         input.style.fontFamily = "serif"
         input.style.background = "rgb(232, 229, 218)"
+        additionInfo.KindWish = `<span style = "margin-left: 26px"> Don't be afraid of new things!❤️</span>`
     }
     else {
         input.style.background = "#CFC5B6"
         input.value = ""
+        additionInfo.KindWish = ""
     }
+
 }
 
 //value - a piece of stored info that can be manipulated by computer
